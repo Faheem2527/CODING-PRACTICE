4687b0eccc1184746f7dfc2bcb702716df8f12ac
Faheem2527 <ec23060@velammalitech.edu.in>
2026-01-29 22:25:21 +0530

    Add files via upload

---
+++ User Input  and Output.py
@@ -0,0 +1,12 @@
+#Input:
+#A = 1, B = 2
+#Output:
+#2
+#Explanation:
+#Multiplication of 1 and 2 is 2.
+
+#User function Template for python3
+class Solution:
+    def multiplication (ob,A,B):
+        # code here 
+        return A*B
